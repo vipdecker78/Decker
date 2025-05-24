a8e4b7585c2317cbda8952cbc859f28f7412123e
vipdecker78 <praiseogbonna83@gmail.com>
2025-05-24 22:40:18 +0100

    Add files via upload
    Signed-off-by: vipdecker78 <praiseogbonna83@gmail.com>

---
+++ config.js
@@ -0,0 +1,43 @@
+/*
+  [ NOMER GANTI PAKE NO BOT KAMU ]
+*/
+global.owner = ['GANTI NOMER KAMU']
+global.ownername = "JustinOfficial"
+global.connect = true // False = QrCode
+global.antilink = false
+global.autotyping = true
+global.welcome = false
+global.autoreadsw = false
+global.autorecord = true
+
+/*
+  [ GAUSAH DI UBAH ]
+*/
+global.mess = {
+"ketua": "*[Justin V17]* Akses Di Tolak!",
+"prem": "*[Justin V17]* Akses Di Tolak!",
+"premium": "*[Justin V17]* Akses Di Tolak!",
+"japost": "*[Justin V17]* Akses Di Tolak!",
+"rekber": "*[Justin V17]* Akses Di Tolak!",
+"owner": "*[Justin V17]* Akses Di Tolak!"
+}
+
+/*
+  [ GAUSAH DI UBAH ]
+*/
+global.tele = "t.me/justinoffc"
+global.tele2 = "t.me/justinoffc"
+global.waMe = "wa.me/6281572074859"
+global.tutorialBot = "https://youtube.com/@justinofficial-id"
+global.versionofscript = "V17.0"
+global.url = "https://files.catbox.moe/cx09ww.jpg"
+global.urlbanner = "https://files.catbox.moe/cx09ww.jpg"
+global.url2 = "https://t.me/justinoffc"
+global.packname = "Stiker By"
+global.author = "JustinOfficial"
+global.group = "https://whatsapp.com/channel/0029VaxxUio545v2bL3FE91g"
+global.idCH = "120363373003239606@newsletter"
+global.xchannel = {
+	jid: '120363373003239606@newsletter'
+	}
+	
